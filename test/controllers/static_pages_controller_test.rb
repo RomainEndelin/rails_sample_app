3abd972986b2f9e4a9b5a65ac6ecca1c4def2648
Romain Endelin <romain@endelin.fr>
2016-04-09 01:34:14 +0200

    Reorder test assertions

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,14 +13,14 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get help" do
     get :help
-    assert_select "title", "Help | #{@base_title}"
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
-    assert_select "title", "About | #{@base_title}"
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
